63bc1ab8d8f0c51ebbe24a4ee4a7eadca14353ad
Shaik Mahaboob Razikh <skrazikh1811@gmail.com>
2025-12-31 15:44:40 +0530

    Create Login.js

---
+++ week 11/fullstack-blog/frontend/src/pages/Login.js
@@ -0,0 +1,16 @@
+import { useAuth } from "../context/AuthContext";
+import { useState } from "react";
+
+export default function Login() {
+  const { login } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div>
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={() => login(email, password)}>Login</button>
+    </div>
+  );
+}
